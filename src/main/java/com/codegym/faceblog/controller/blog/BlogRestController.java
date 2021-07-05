0c4505e93efd3ea76d580f948e506241facd1b0d
Hieucodegym <vuhieu628383@gmail.com>
2021-07-05 15:41:55 +0700

    update ajax index

--- src/main/java/com/codegym/faceblog/controller/blog/BlogRestController.java
+++ src/main/java/com/codegym/faceblog/controller/blog/BlogRestController.java
@@ -1,5 +1,11 @@
 package com.codegym.faceblog.controller.blog;
 
+import com.codegym.faceblog.model.Blog;
+import com.codegym.faceblog.service.blog.BlogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/blogs")
 public class BlogRestController {
 
+    @Autowired
+    private BlogService blogService;
+
+    @GetMapping("")
+    public ResponseEntity<Iterable<Blog>> showListBlog(){
+        return new ResponseEntity<>(blogService.findAll(), HttpStatus.OK);
+    }
 }
