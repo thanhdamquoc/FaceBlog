de40717c6b694193bc6770d6a1f5a7ec67235744
thanhdamquoc <79950542+thanhdamquoc@users.noreply.github.com>
2021-07-05 10:34:34 +0700

    security configuration and related testing done

--- src/main/java/com/codegym/faceblog/service/user/UserServiceImpl.java
+++ src/main/java/com/codegym/faceblog/service/user/UserServiceImpl.java
@@ -7,6 +7,7 @@ import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -15,6 +16,9 @@ import java.util.Optional;
 public class UserServiceImpl implements UserService{
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public Iterable<User> findAll() {
@@ -28,6 +32,7 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public User save(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
