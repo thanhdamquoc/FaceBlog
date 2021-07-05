5f60c509b78a658e89dbb75039de80eeee75b387
kienk93 <kienk93@users.noreply.github.com>
2021-07-05 10:46:32 +0700

    add CRUD cơ bản comment + commentReaction

--- src/main/java/com/codegym/faceblog/controller/comment/commentController.java
+++ src/main/java/com/codegym/faceblog/controller/comment/commentController.java
@@ -1,17 +1,9 @@
 package com.codegym.faceblog.controller.comment;
 
-import com.codegym.faceblog.model.Comment;
 import com.codegym.faceblog.service.blog.BlogService;
-import com.codegym.faceblog.service.comment.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Optional;
 
