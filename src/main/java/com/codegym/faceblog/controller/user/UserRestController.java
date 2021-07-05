f32b889e0ddb612446da7612e3b6b2bfbdf49dbe
thanhdamquoc <79950542+thanhdamquoc@users.noreply.github.com>
2021-07-05 22:33:29 +0700

    add validation feature for sign up form

--- src/main/java/com/codegym/faceblog/controller/user/UserRestController.java
+++ src/main/java/com/codegym/faceblog/controller/user/UserRestController.java
@@ -1,6 +1,8 @@
 package com.codegym.faceblog.controller.user;
 
+import com.codegym.faceblog.model.Role;
 import com.codegym.faceblog.model.User;
+import com.codegym.faceblog.service.role.RoleService;
 import com.codegym.faceblog.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,6 +15,9 @@ public class UserRestController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private RoleService roleService;
+
     @GetMapping
     private ResponseEntity<Iterable<User>> findAll() {
         Iterable<User> users = userService.findAll();
@@ -24,6 +29,7 @@ public class UserRestController {
 
     @PostMapping
     private ResponseEntity<User> addUser(@RequestBody User user) {
+        user.setRole(roleService.findByName("ROLE_USER"));
         return new ResponseEntity<>(userService.save(user), HttpStatus.CREATED);
     }
 }
--- src/main/java/com/codegym/faceblog/model/User.java
+++ src/main/java/com/codegym/faceblog/model/User.java
@@ -3,6 +3,7 @@ package com.codegym.faceblog.model;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.Pattern;
 
 @Entity
 @Data
@@ -12,8 +13,10 @@ public class User {
     private Long id;
 
     @Column(unique = true)
+//    @Pattern(regexp = "^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$")
     private String username;
 
+//    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{8,}$")
     private String password;
 
     private String fullName;
