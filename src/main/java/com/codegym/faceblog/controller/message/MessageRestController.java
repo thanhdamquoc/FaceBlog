51813aaabfd3c2ad743bd091e503f94a2900905f
thanhdamquoc <79950542+thanhdamquoc@users.noreply.github.com>
2021-07-07 14:18:21 +0700

    finish messaging feature, auto scroll down not added

--- src/main/java/com/codegym/faceblog/controller/message/MessageRestController.java
+++ src/main/java/com/codegym/faceblog/controller/message/MessageRestController.java
@@ -1,14 +1,12 @@
 package com.codegym.faceblog.controller.message;
 
 import com.codegym.faceblog.model.Message;
+import com.codegym.faceblog.model.User;
 import com.codegym.faceblog.service.message.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 
@@ -22,5 +20,14 @@ public class MessageRestController {
     public ResponseEntity<Message> saveMessage(@RequestBody Message message) {
         message.setDate(new Date());
         return new ResponseEntity<>(messageService.save(message), HttpStatus.OK);
+    }
+
+    @GetMapping("/user/{senderId}/user/{receiverId}")
+    public ResponseEntity<Iterable<Message>> getPrivateMessages(@PathVariable Long senderId, @PathVariable Long receiverId) {
+        Iterable<Message> messages = messageService.getPrivateMessages(senderId, receiverId);
+        if (!messages.iterator().hasNext()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(messages, HttpStatus.OK);
     }
 }
