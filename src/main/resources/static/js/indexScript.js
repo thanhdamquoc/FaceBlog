2ff3a24f0002dd6b3a1a1e3b4ba7b6814331736f
Hieucodegym <vuhieu628383@gmail.com>
2021-07-11 22:10:41 +0700

    update feature display count notification, bug display contetn notification

--- src/main/resources/static/js/indexScript.js
+++ src/main/resources/static/js/indexScript.js
@@ -228,6 +228,7 @@ function checkNewReaction(blog, userIdReact) {
     if (userIdBlog == userIdReact) {
         content = notificationBody(1)
         document.getElementById("notification").innerHTML = content;
+        renderDropDownNotification(blog);
     } else {
         content = notificationBodyWithoutReact()
         document.getElementById("notification").innerHTML = content;
@@ -240,19 +241,11 @@ function notificationBody(number) {
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bell-fill" viewBox="0 0 16 16">
                 <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z"/>
             </svg>
-            <span id='spanNotification' style="width: 17px;height: 17px;position: absolute;text-align: center;border-radius: 50%;background: red;color: white;right: 2px;top: 4px;">
+            <div id="countNotification" style="width: 17px;height: 17px;position: absolute;text-align: center;border-radius: 50%;background: red;color: white;right: 2px;top: 4px;">
                 ${number}
-            </span>
-            <ul class="dropdown-menu">
-                <li>
-                    <a href="#">
-                    <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                          fill="currentColor" class="bi bi-gear-fill" viewBox="0 0 16 16">
-                          <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/>
-                          </svg>
-                    </span>
-                    Setting</a>
-                </li>
+            </div>
+            <ul class="dropdown-menu" id="contentNotification">
+                
             </ul>
         </a>`
 }
@@ -277,11 +270,23 @@ function notificationBodyWithoutReact() {
         </a>`
 }
 
-function renderDropDownNotification() {
-    hideNotification();
-}
-function hideNotification() {
-    document.getElementById('spanNotification').style.display = "block|none";
+function renderDropDownNotification(blog) {
+    document.getElementById("countNotification").style.display = "none";
+    let blogId = blog.id;
+    let url = "/blogs/" + blogId + "/blog-reactions";
+    $.ajax({
+        type: "GET",
+        url: url,
+        success: function (blogReactions) {
+            let content = "";
+            for (let i = 0; i < blogReactions.length; i++) {
+                content += `<li>
+                               <a href="#">${blogReactions[i].user.username} <span> đã thích bài của bạn!</span></a>
+                           </li>`
+            }
+            $('#contentNotification').html(content);
+        }
+    });
 }
 
 //Messaging Feature
